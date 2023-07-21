b91f8d607d4ec86a18a99edbb974771eb9b2892a
alfonsouzcategui2@gmail.com <alfonsouzcategui2@gmail.com>
2023-07-20 20:15:04 -0500

    modify

--- backend/src/functions/initProject.comand.js
+++ backend/src/functions/initProject.comand.js
@@ -1,19 +1,49 @@
 const { exec } = require('child_process');
 
+const initProject = (urlRepository, project, dir, install, prevStart, cmdStart) => {
+  cloneRepositoryInServer();
+  dependencyInstall();
+  runProject();
+}
 
 
-const clonarRepositorioEnServidor = (urlRepositorio, project) => {
+
+const cloneRepositoryInServer = (urlRepository, project, dir) => {
 
   const path = `/home/glya/projects/${project}`
 
   // Comando para clonar el repositorio usando Git
-  const comando = `git clone ${urlRepositorio} ${path}`;
+  const cmd = `git clone ${urlRepository}${dir && dir} ${path}`;
+  
+  const result = { path, ...ejectCmd(cmd)};
+  
+  return result;
+}
 
-  exec(comando, (error, stdout, stderr) => {
+const dependencyInstall = async (project, install) => {
+  
+  // Comando para entrar al proyecto
+  const cmd = `cd /home/glya/projects/${project}`;
+
+  await ejectCmd(cmd);
+  
+  const result = await ejectCmd(install);
+  
+  return result;
+}
+
+const runProject = (prevStart, cmdStart) => {
+  const cmd = `${prevStart ? prevStart + '&&' : ''}${cmdStart}`;
+  
+  return ejectCmd(cmd);
+}
+
+const ejectCmd = cmd => {
+exec(cmd, (error, stdout, stderr) => {
     if (error) {
-      console.error('Error al clonar el repositorio:', error);
+      console.error(error);
     } else {
-      return { path, salida: stdout};
+      return { salida: stdout };
     }
   });
 }
